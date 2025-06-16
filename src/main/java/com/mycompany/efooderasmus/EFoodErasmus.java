576ab3585f4916551caf386afe624df6b58e1ec0
root <root@Mintxo-Windows-11>
2025-06-16 18:51:22 +0300

    va

---
+++ src/main/java/com/mycompany/efooderasmus/EFoodErasmus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.efooderasmus;
+
+/**
+ *
+ * @author mintxo
+ */
+public class EFoodErasmus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
